aaac42eb92ed1375398b130600184c666b5a3fc8
Reuben Ramos <rsramos23@gmail.com>
2022-12-16 09:04:50 -0800

    heroku fix again

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://muttycapsco-app.herokuapp.com";
+const BASE_URL = "https://muttycapsco-app.herokuapp.com/api";
 
 export const api = axios.create({
   baseURL: `${BASE_URL}`,
